deaceba35d3af34c5e6583ba44cfc2a26c525846
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-15 19:30:00 +0500

    Add files via upload

---
+++ 02_Single & Multilines Comments.py
@@ -0,0 +1,13 @@
+print("******Single Line Comment*******")
+# Single line comments start with a hash (#) symbol.
+# Everything after the hash on that line is ignored by the interpreter.
+print("This line will be executed.")  # This comment is ignored
+
+print("****Double Line Comment****")
+# Double line comments is created by using triple quotes (''' or """).
+"""
+This is a multi-line comment.
+It can span multiple lines.
+The interpreter ignores everything within the triple quotes.
+"""
+print("This line will also be executed.")# This comment is ignored
