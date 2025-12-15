931c8bd912b068e1a79c03990c5024ccca3fc1b4
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-15 19:30:24 +0500

    Add files via upload

---
+++ 03_pip_install_&_Execution.py
@@ -0,0 +1,7 @@
+# I have installed pandas using pip and now I will import it to verify the installation.
+import pandas as pd
+# Create a simple DataFrame to check if pandas works
+data = {
+    'Name': ['Haris', 'Ali', 'Usman'],
+    'Score': [90, 85, 88]
+}
