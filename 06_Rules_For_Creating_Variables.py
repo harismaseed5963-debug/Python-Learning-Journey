4592544d386b1b4b9f5bb0245114603cc8e4d784
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-16 14:30:17 +0500

    Add files via upload

---
+++ 06_Rules_For_Creating_Variables.py
@@ -0,0 +1,10 @@
+# A variable name must start with a letter (a-z, A-Z) or an underscore (_)
+# A variable name cannot start with a number (0-9)
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# For Example:
+a = 10        # Valid variable name
+_b = 20       # Valid variable name
+c3 = 30       # Valid variable name
+#  3d = 40     # Invalid variable name (starts with a number)
+# my-variable = 50  # Invalid variable name (contains a hyphen)
