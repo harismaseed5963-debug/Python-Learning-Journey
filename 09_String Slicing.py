4c11a1acad2d7ebfeb27d08993f63d8da74747a5
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-17 14:39:28 +0500

    Add files via upload

---
+++ 09_String Slicing.py
@@ -0,0 +1,12 @@
+# Print a single character from a string using indexing
+name = "Haris"
+nameshort = name[3]
+print(nameshort)
+
+# Print a substring from a string using slicing
+shortname = name[0:9]
+print("Your Short Name Is : ", shortname)
+
+# There are two types of String Slicing 
+# 1. Positive Slicing(Starts From 0)
+# 2. Negative Slicing(Starts From -1)
