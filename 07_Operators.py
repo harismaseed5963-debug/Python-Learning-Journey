9a13a3b20f31874638e3e54086e63039317e881a
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-17 14:38:45 +0500

    Add files via upload

---
+++ 07_Operators.py
@@ -0,0 +1,23 @@
+# Assignment Operators
+a = 4+3
+a += 5
+print(a)
+b = 7-5
+b -= 7
+print(b)
+# Comparison Operators
+f = 5>=7
+print(f)
+g = 8<3
+print(g)    
+h = 6==6
+print(h)
+i = 9!=2
+print(i)
+# Logical Operators
+j = (5>3) and (8<6)
+print(j)
+k = (4==4) or (7>9)
+print(k)
+l = not(3<=1)
+print(l)
