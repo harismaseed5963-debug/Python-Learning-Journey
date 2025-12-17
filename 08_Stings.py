23ae2a13d2880551219c9fabe175ea518c7f3c73
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-17 14:39:06 +0500

    Add files via upload

---
+++ 08_Stings.py
@@ -0,0 +1,7 @@
+name = "Haris Ur Rehman" #The Data Inside the Double Quoted  Is called string.
+# Three Ways of Representing Stings
+a = "Haris" #Data Inside Double Quotes
+b = 'Haris' #Data Inside Single Quotes
+c = ''' Haris ''' # Data Inside Triple Quotes
+# In Strings The Start Index Is included in the result 
+# But the End Index Is Not Included Into the result
