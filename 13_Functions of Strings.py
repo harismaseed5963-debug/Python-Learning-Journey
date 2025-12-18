04c5b34a8b5b3211ac036531e1582b7dc99ac450
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-18 19:09:11 +0500

    Add files via upload

---
+++ 13_Functions of Strings.py
@@ -0,0 +1,10 @@
+# 1.Length Fuction(len)
+name = "Haris"
+print(len(name))
+# 2. EndsWith Function
+print(name.endswith("ris"))
+# 3. Startswith Function
+print(name.startswith("Ha"))
+# 4. Capitalize Fuction
+print(name.capitalize())
+# There are alot of strings functions in python like lower(), upper(), find(), replace() etc.
