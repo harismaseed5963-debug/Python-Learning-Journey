e50fa47022f8a3f9b21b7bfd45f1b7343e44efcb
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-18 19:11:48 +0500

    Add files via upload

---
+++ 15_F string Function.py
@@ -0,0 +1,7 @@
+# F string Function in Python
+name = input("Enter Your Name ")
+# It is a way to format strings in Python using f-strings
+print(f"Specially Welcome to {name}")
+# You can also use expressions inside f-strings
+age = int(input("Enter Your Age "))
+print(f"You will be {age + 1} years old next year.")
