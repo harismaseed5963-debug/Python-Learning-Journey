4a295dfaec8afde062a77d3b5a3bc0916d06a674
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-21 19:55:36 +0500

    Add files via upload

---
+++ 16_Lists.py
@@ -0,0 +1,6 @@
+# Lists Syntex => The Data/Values Must be in between of Square Brackets[].
+# For Example = Fruits = ["Apples","Bananas","Orange"]
+Colors = ["Red","White","Blue","Black"]
+print(Colors[1])
+print(Colors[3])
+print(Colors[0:4])
