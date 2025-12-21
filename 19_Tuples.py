01d41505ff845be164b853bf86a27b1165efd77e
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-21 19:56:26 +0500

    Add files via upload

---
+++ 19_Tuples.py
@@ -0,0 +1,10 @@
+Games = ("Cricket","Football","Hockey","Voleyball","Basketball")
+print(type(Games))
+# Empty Tupple Syntex
+Empty_Tupple = ()
+# Single Values Syntex (Comma Is must after the single value)
+Signal_Values_Tupple = (1,)
+# Multiple Values Syntex
+Multiple_Values_Tupple = (1,2,"Game",False)
+print(Games[2])
+print(Games[0:3])
