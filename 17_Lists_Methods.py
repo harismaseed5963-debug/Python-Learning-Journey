d91595b48b700b3569042b333539b83f54603c0d
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-21 19:55:55 +0500

    Add files via upload

---
+++ 17_Lists_Methods.py
@@ -0,0 +1,19 @@
+Colors = ["Red","White","Blue","Black"]
+# 1. .append It is used to add the data or attach the data at the end.
+Colors.append("Yellow")
+print(Colors)
+# 2. .sort It will ordered your Your Data.
+Colors.sort()
+print(Colors)
+# 3. .reverse It will reverse the order of the list
+Colors.reverse()
+print(Colors)
+# 4. .insert It will give you option that at which index you will add the data in the list
+Colors.insert(3,"Green")
+print(Colors)
+# 5. .remove It will remove the data from the list
+Colors.remove("Black")
+print(Colors)
+# 6. .pop It will Remove the value by giving the index of the value in a list.
+(Colors.pop(3))
+print(Colors)
