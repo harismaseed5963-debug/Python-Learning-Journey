51a97d8cdd0723035bb2eea39a6d7e93c6282eab
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-21 19:56:43 +0500

    Add files via upload

---
+++ 20_Operations_In_Tupples.py
@@ -0,0 +1,13 @@
+# Tupple Concatenation
+Tupple1 = (1,2,3)
+Tupple2 = ("a","b","c")
+Tupple3 = Tupple1 + Tupple2
+print(Tupple3)
+# Tupple Repetition
+Tupple4 = Tupple1 * 3
+print(Tupple4)
+# MemberShip Test
+t = (10, 20, 30)
+print(20 in t)     # True
+print(40 not in t) # True
+print(50 in t) # False
