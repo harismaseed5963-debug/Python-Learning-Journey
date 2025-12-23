e55f62a076c7ae596cc87d7cadde6d6ff09dc3c5
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-23 12:13:40 +0500

    Add files via upload

---
+++ 22_Dictioneries.py
@@ -0,0 +1,14 @@
+# A Dictionary Must be Enclosed in Curly Braces; 
+# Comma is Must In the end of the values
+marks = {} # These is an Empty Dictionary
+Dictionries = {
+    "Haris" : 55,
+    "Hasher" : 66,
+    "Kashif" : 73,
+}
+print(Dictionries["Haris"])
+print(Dictionries["Hasher"])
+
+# "Haris" : 55 => Here "Haris" Is a Key & 55 is a value
+# The Value before the colon is key & value after colon is a value
+# We can store every type of data within the dictionry
