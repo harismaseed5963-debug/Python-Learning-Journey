efc0c10957cae9e9543b01df0db49f0fb8eb9f55
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-23 12:15:15 +0500

    Add files via upload

---
+++ 23_Methods_OF_Dictionries.py
@@ -0,0 +1,17 @@
+marks = {
+    "Haris" : 55,
+    "Hasher" : 66,
+    "Kashif" : 73,
+}
+# Methods :
+# 01 How To Print All the values of Dict
+print(marks.values())
+# 02 How to print All the keys of Dict
+print(marks.keys())
+# 03 How to print All the items of Dict
+print(marks.items())
+# 04 How to update the Dict
+marks.update({"Haris": 98})
+print(marks)
+# 05 How to get the data of a specific key
+print(marks.get("Haris"))
