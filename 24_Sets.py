a2b4d0645207b022e2b92c69eb951b50672f98f8
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-25 13:56:35 +0500

    Add files via upload

---
+++ 24_Sets.py
@@ -0,0 +1,7 @@
+# Inside the set the repitative value is print only one time 
+# In below example the 5 is two time in the set and it is printed only one time 
+my_set = {5, 5, 6, 7} # We only kept Values within the set.
+print(my_set) 
+# How to Create an empty Set?
+Empty_Set = set() # Don't Use {} For Creating empty set
+print(type(Empty_Set))
