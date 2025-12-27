e57a9ddb3ac88bf97dcda58772ee6ce1f6cf9b9f
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2025-12-27 14:17:56 +0500

    Fix indentation and operator syntax in eligibility check

--- 27_Else_if_Funtion.py
+++ 27_Else_if_Funtion.py
@@ -1,16 +1,16 @@
 # Taking Name and Age as Input from User and Print Eligibility Message
 marks = int(input("Enter Your Marks : "))
 name = input("Enter Your Name : ")
-if 90 <= marks >= 100:
+if 90 <= marks <= 100:
     grade = "A++"
     performance = "Excellent"
-elif 80 <= marks >= 89:
+elif 80 <= marks <= 89:
     grade = "A"
     performance = "Very Good"
-elif 70 <= marks >= 79:
+elif 70 <= marks <= 79:
     grade = "B"
     performance = "Good"
-elif 50 <= marks >= 69:
+elif 50 <= marks <= 69:
     grade = "C"
     performance = "Average"
 else:
@@ -20,4 +20,5 @@ else:
 print("----- Your Result -----")
 print(f"Marks : {marks}")
 print(f"Grade =  {grade}")
+
 print(f"Performance =  {performance}")
