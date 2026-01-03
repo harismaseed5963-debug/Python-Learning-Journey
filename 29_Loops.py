eabd3494af24c22541e7ac97d31ac91a76d49e89
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-01-03 09:50:51 +0500

    Add files via upload

---
+++ 29_Loops.py
@@ -0,0 +1,11 @@
+# Loop Means To repeat a function again and again icluding changes
+# Print Numbers From 0 to 5 Using Loops
+print("# Print Numbers From 0 to 5 Using Loops")
+for i in range(6):
+    print(i)
+# Print Numbers from 3 to 9
+print("Print Numbers from 3 to 9")
+i = 3
+while i<=9:
+    print(i)
+    i += 1
