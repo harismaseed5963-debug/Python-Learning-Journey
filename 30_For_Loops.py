0416ea9a5aa6643e4ef89a6bc082c8eb78e7950f
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-01-03 09:51:08 +0500

    Add files via upload

---
+++ 30_For_Loops.py
@@ -0,0 +1,11 @@
+# We Use 'For Loops' When we know in advance that how many times want to repeat
+for i in range(1,10):
+    print(i)
+    i += 1
+print("Print Even Numbers Between 1 To 10 Through Using For Loop")
+for i in range(0, 10,2):
+    print(i)
+print("Print Odd Numbers Between 1 To 10 Through Using For Loop")
+for i in range(1,10,2):
+    print(i)
+    
