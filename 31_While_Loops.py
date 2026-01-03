07fc2999eafc86e557e98ccd7460195ca074191d
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-01-03 09:51:23 +0500

    Add files via upload

---
+++ 31_While_Loops.py
@@ -0,0 +1,8 @@
+# We Use While loop When we don't know that how many times we will repeat the process
+# While loop make execution until the condition becomes false
+# Print  Numbers Between 1 to 20.
+i = 1
+while i <= 20:
+    print(i)
+    i += 1
+    
