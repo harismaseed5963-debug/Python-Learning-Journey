3249e3834de48c1935ef21f39f7b759a2790c6d9
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-01-13 19:42:39 +0500

    Add files via upload

---
+++ 33_Funtion Definition.py
@@ -0,0 +1,8 @@
+# Funtion is Consist of three main parts
+# 1. Funtion Definition
+# 2. Funtion Call
+# 2. Funtion Call
+# Funtion Definition:
+# In Funtion Definition we define the name of the funtion and its parameters if any.
+def greet(name="Haris"):# Funtion Definition
+    print('Hello ',name)# Funtion Body
