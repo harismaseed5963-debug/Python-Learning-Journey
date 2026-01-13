27e847b16682f0c8e170daded1781b2eca495bb3
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-01-13 19:42:17 +0500

    Add files via upload

---
+++ 32_Funtions.py
@@ -0,0 +1,14 @@
+# Functions in Python
+# A function is a block of reusable code that performs a specific task.
+# Functions help in organizing code, improving readability, and avoiding repetition.
+# Funtions have three main parts:
+# 1. Function Definition 
+# 2. Function Call
+# 3. Function Body
+# For example:
+# Function Definition
+def greet(name="Haris"):
+# Function Body
+    print('Hello ',name)
+# Function Call
+greet()
