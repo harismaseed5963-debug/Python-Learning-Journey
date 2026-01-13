0bed3d4a85c180f3e0fd4ac8d370f80a42f9325d
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-01-13 19:43:13 +0500

    Add files via upload

---
+++ 35_Average_Function.py
@@ -0,0 +1,17 @@
+# Creating Average Function in Python
+# Define the Average Funtion
+def avg():
+    a = int(input("Enter Your First Value : "))
+    b = int(input("Enter Your First Value : "))
+
+    average = (a+b)/2
+    print(average)
+# Call The Average Function
+avg()
+print("Well Done")
+avg()
+print("Well Done")
+avg()
+print("Well Done")
+avg()
+print("Well Done")
