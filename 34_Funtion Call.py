13f19c8bfd747afd67068835b14b4b3edae50b9f
Haris Ur Rehman <azlanmaseed5963@gmail.com>
2026-01-13 19:42:58 +0500

    Add files via upload

---
+++ 34_Funtion Call.py
@@ -0,0 +1,8 @@
+# What is Function Call?
+# Function Call is the process of executing a function that has been defined earlier.
+# When a function is called, the program control is transferred to the function's code block,
+# and the code inside the function is executed.
+# Function Call:
+def greet(name="Haris"):# Function Definition
+    print('Hello ',name)# Function Body
+greet()# Function Call
